03ac817444be2ae1bcee325c732da2d57a9097c8
zaidoi <zaidbadgujar7@gmail.com>
2026-05-17 00:50:35 +0530

    Signup and Login page completed

--- apps/frontend/src/pages/Login.tsx
+++ apps/frontend/src/pages/Login.tsx
@@ -1,9 +1,46 @@
-import React from 'react'
+
 import Form from '../components/Form'
+import axios from 'axios'
+import type { SignupInput } from '@repo/common/src/zod/models'
 
 const Login = () => {
+
+  
+
+  const onSubmit = async (data:SignupInput) => {
+   
+    try {
+      const res = await axios.post("http://localhost:3000/user/login",data);
+
+      localStorage.setItem('token',res.data.token)
+      console.log("worked")
+      console.log(res.data.token)
+    } catch (error) {
+      if(error instanceof Error){
+        console.log(error.message)
+      }
+    }
+  }
+
+
+
   return (
-    <div><Form type='signup'/></div>
+    <div className='grid md:grid-cols-2 h-screen'>
+
+      <div className='flex justify-center items-center'>
+        <Form type='login' onSubmit={onSubmit}/>
+      </div>
+
+      <div className='flex justify-center items-center'>
+
+       <div className='hidden md:block'>
+         <h1 className='text-4xl font-bold'>Ready to connect to the <span className='text-amber-600'>WORLD!</span></h1>
+        <p>--Medium</p>
+       </div>
+
+      </div>
+
+    </div>
   )
 }
 
